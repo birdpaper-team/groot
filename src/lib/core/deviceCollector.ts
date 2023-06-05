0c57ec130eb2ab4531aa297b35fa70a205d38d02
Sam <llh@birdpaper.com>
2023-06-05 15:16:58 +0800

    Feat(core):

--- src/lib/core/deviceCollector.ts
+++ src/lib/core/deviceCollector.ts
@@ -1,12 +1,24 @@
 import collector from "../../entity/collector";
+import { DEVICE_TYPE } from "../../types/device";
 import logger from "../../utils/log";
 
 /**
  * 设备采集器
  */
 export default class deviceCollector extends collector {
+  /**
+   * 获取设备类型
+   * @returns {DEVICE_TYPE} 设备类型
+   */
+  public getDeviceType(): DEVICE_TYPE {
+    const userAgentData = navigator["userAgentData"];
+    const { platform } = userAgentData;
+    return platform;
+  }
+
   public install() {
     logger.info(`[ DeviceCollector ] - Installed`);
     // const data = navigator;
+    console.log("[  ]-47", this.getDeviceType());
   }
 }
---
+++ src/types/device.ts
@@ -0,0 +1,70 @@
+/** 终端系统类型 */
+export enum DEVICE_TYPE {
+  Android = "Android",
+
+  Windows = "Windows",
+
+  Mac = "Mac",
+
+  iPhone = "iPhone",
+
+  iPad = "iPad",
+
+  unknown = "Unknow",
+}
+
+/** 系统类型 */
+export enum SYSTEM_TYPE {
+  Android = "Android",
+
+  Windows = "Windows",
+
+  Mac = "Mac",
+
+  IOS = "IOS",
+}
+
+/** 浏览器类型 */
+export enum BROWSER_TYPE {
+  /** IE 浏览器 */
+  IE = "IE",
+
+  /** Chrome 浏览器 */
+  Chrome = "Chrome",
+
+  /** FireFox 浏览器 */
+  FireFox = "FireFox",
+
+  /** Opera 浏览器 */
+  Opera = "Opera",
+
+  /** Safari 浏览器 */
+  Safari = "Safari",
+
+  /** Edge 浏览器 */
+  Edge = "Edge",
+
+  /** QQBrowser 浏览器 */
+  QQBrowser = "QQBrowser",
+
+  /** WeixinBrowser */
+  WeixinBrowser = "WeixinBrowser",
+
+  /** UCBrowser */
+  UCBrowser = "UCBrowser",
+
+  /** UCBrowser */
+  BaiDuBrowser = "UCBrowser",
+
+  /** SouGouBrowser */
+  SouGouBrowser = "SouGouBrowser",
+
+  /** Browser360 */
+  Browser360 = "Browser360",
+
+  /** 钉钉浏览器 */
+  DingDing = "DingDing",
+
+  /** 未知浏览器 */
+  UNKNOWN = "UNKNOWN",
+}
